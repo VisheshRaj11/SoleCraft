f4852eeb95dc0c5c79ae23b75d94c72af58fb5b5
Vishesh Raj <visheshrajput47@gmail.com>
2025-12-25 21:39:44 +0530

    do some changes

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -2,6 +2,7 @@ import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from '../models/User.js';
 
+
 // No need to connect MongoDB here - it will be connected in server.js
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
